b1871235b7952f6bf7eb4029ecb7373e692e70d0
Sammit Dhar <sammit.dhar.2004@gmail.com>
2025-09-13 03:39:08 +0530

    Create fifo_pkg package with UVM includes

---
+++ package.sv
@@ -0,0 +1,14 @@
+package fifo_pkg;
+  import uvm_pkg::*;
+  `include "uvm_macros.svh"
+
+  `include "seq_item.sv"
+  `include "base_seq.sv"
+  `include "sequencer.sv"
+  `include "driver.sv"
+  `include "monitor.sv"
+  `include "scoreboard.sv"
+  `include "agent.sv"
+  `include "env.sv"
+  `include "base_test.sv"
+endpackage
